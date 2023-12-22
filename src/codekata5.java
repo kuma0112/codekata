efaaa23e88de47b1a20e30d2552d4d8978daf228
kuma0112 <lsj23871@gmail.com>
2023-12-22 09:50:47 +0900

    231222 first commit

---
+++ src/codekata5.java
@@ -0,0 +1,13 @@
+public class codekata5 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+
+        if (num1 == num2)
+            answer = 1;
+
+        if (num1 != num2)
+            answer = -1;
+
+        return answer;
+    }
+}

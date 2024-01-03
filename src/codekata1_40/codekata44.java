49de5fbbbff428ac34d09882484b3476f79f6b0d
kuma0112 <lsj23871@gmail.com>
2024-01-03 09:57:20 +0900

    240103 코드카타 44번 최소 직사각형

---
+++ src/codekata1_40/codekata44.java
@@ -0,0 +1,19 @@
+package codekata1_40;
+
+public class codekata44 {
+    public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        // 각 명함의 가로와 세로 중 더 긴 쪽을 maxWidth, 더 짧은 쪽을 maxHeight로 설정
+        for (int[] size : sizes) {
+            int width = size[0];
+            int height = size[1];
+
+            maxWidth = Math.max(maxWidth, Math.max(width, height));
+            maxHeight = Math.max(maxHeight, Math.min(width, height));
+        }
+
+        return maxWidth * maxHeight;
+    }
+}

46087381b7eed3e52732cdca1b1e2a25ddb0c0b9
kuma0112 <lsj23871@gmail.com>
2024-02-02 14:46:16 +0900

    240202 코드카타 53번 명예의 전당(1)

--- src/codekata53.java
+++ src/codekata53.java
@@ -1,22 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class codekata53 {
-    public List<Integer> solution(int k, int[] score) {
-        int min = score[0];
-        List<Integer> answer = new ArrayList<>();
+        public List<Integer> solution(int k, int[] score) {
+            List<Integer> answer = new ArrayList<>();
+            List<Integer> array = new ArrayList<>();
 
-        // 1. k 길이 만큼의 배열을 만든다.
-        // 2. int[] score의 값을 k 배열에 집어넣는다.
-        // 3. k 배열의 순서는 계속 유지된다. 가장 작은 값이 인덱스 0번.
-        // 4. answer에는 k 인덱스 0번이 들어간다.
-        for (int i = 0; i < k; i++){
-            if (score[i] < min){
-                min = score[i];
+            for (int index = 0; index < score.length; index++) {
+                // 1. 0, 0~1, 0~2 이런 식으로 더해가며 가수 점수를 array에 추가
+                array.add(score[index]);
+
+                // 2. 만약 오늘의 날짜가 k보다 클 경우, 가장 낮은 점수를 제거 (k일 이후부터는 array의 크기가 k로 유지되어야 하기 때문)
+                // Collections.binarySearch는 정렬된 리스트에서 특정 값을 찾는 데 사용되며, 찾은 경우 해당 요소의 인덱스를 반환하고 찾지 못한 경우 음수를 반환한다.
+                // 다시 말해, scroe[index-k]를 가장 최소값이라고 생각하고 이것이 현재의 array에서 발견되면 삭제한다.
+                if (index >= k) {
+                    array.remove(Collections.binarySearch(array, score[index - k]));
+                }
+
+                answer.add(array.get(k - 1));
+
+                Collections.sort(array);
             }
-        }
-
-
-        return answer;
+            return answer;
     }
 }

cd4874b2ea48ff5dfde6b4b5f2205e145d476a91
kuma0112 <lsj23871@gmail.com>
2024-02-05 14:34:42 +0900

    240205 코드카타 53번 명예의 전당(1)

--- src/codekata53.java
+++ src/codekata53.java
@@ -1,27 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class codekata53 {
-        public List<Integer> solution(int k, int[] score) {
-            List<Integer> answer = new ArrayList<>();
-            List<Integer> array = new ArrayList<>();
+    public int[] solution(int k, int[] score) {
+        // 1. answer 배열 초기화
+        int[] answer = new int[score.length];
 
-            for (int index = 0; index < score.length; index++) {
-                // 1. 0, 0~1, 0~2 이런 식으로 더해가며 가수 점수를 array에 추가
-                array.add(score[index]);
+        // 2. 우선순위 큐를 사용해본다.
+        // 최하위 점수에 쉽게 접근할 수 있으며 새로운 점수를 추가하거나 최하위 점수를 제거하는 작업이 쉽게 가능하다.
+        // 먼저, 우선순위 큐를 초기화한다.
+        PriorityQueue<Integer> hallOfFame = new PriorityQueue<>();
 
-                // 2. 만약 오늘의 날짜가 k보다 클 경우, 가장 낮은 점수를 제거 (k일 이후부터는 array의 크기가 k로 유지되어야 하기 때문)
-                // Collections.binarySearch는 정렬된 리스트에서 특정 값을 찾는 데 사용되며, 찾은 경우 해당 요소의 인덱스를 반환하고 찾지 못한 경우 음수를 반환한다.
-                // 다시 말해, scroe[index-k]를 가장 최소값이라고 생각하고 이것이 현재의 array에서 발견되면 삭제한다.
-                if (index >= k) {
-                    array.remove(Collections.binarySearch(array, score[index - k]));
-                }
+        for (int day = 0; day < score.length; day++){
+            // 3. 현재 가수의 점수를 우선순위 큐에 추가한다.
+            hallOfFame.add(score[day]);
 
-                answer.add(array.get(k - 1));
-
-                Collections.sort(array);
+            // 4. k일 이후부터는 큐의 size를 k로 유지해야 하므로,
+            // 우선순위 큐의 크기가 k를 초과하면 가장 낮은 점수를 제거한다.
+            if (day >= k) {
+                hallOfFame.poll();
             }
-            return answer;
+
+            // 5. 현재 명예의 전당 배열에 최하위 점수를 저장
+            answer[day] = hallOfFame.peek();
+        }
+        return answer;
     }
 }

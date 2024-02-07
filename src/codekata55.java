148c33b4789fcfd3d0eeeb07ad52554bcf5188f9
kuma0112 <lsj23871@gmail.com>
2024-02-07 14:40:04 +0900

    240207 코드카타 55번 카드 뭉치

---
+++ src/codekata55.java
@@ -0,0 +1,14 @@
+public class codekata55 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+
+        // 1. 향상된 for문으로 goal의 단어 하나하나를 돈다.
+
+        // 2. 이렇게 도는 동안 cards1에서 같은 단어를 만나면 집어 넣는다.
+
+        // 3. cards1에 같은 단어가 없으면 cards2를 뒤져 같은 단어를 집어 넣는다.
+
+        // 4. 그렇게 해서 완성된 새로운 배열이 goal과 같으면 "Yes"를, 아니면 "No"를 반환한다.
+        return answer;
+    }
+}

e6ead727ea98b91c75c89a6e0a3acb2eb8e9d4c8
kuma0112 <lsj23871@gmail.com>
2024-02-08 14:40:14 +0900

    240208 코드카타 55번 카드 뭉치

--- src/codekata55.java
+++ src/codekata55.java
@@ -1,14 +1,30 @@
+import java.util.HashSet;
+
 public class codekata55 {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
+        HashSet<String> usedWords = new HashSet<>();
+        int index = 0;
 
-        // 1. 향상된 for문으로 goal의 단어 하나하나를 돈다.
+        for (String word : goal) {
+            // 1. 이미 사용한 단어인지 확인한다.
+            if (usedWords.contains(word)) {
+                return "No"; // 이미 사용한 단어일 경우 "No" 반환한다.
+            }
 
-        // 2. 이렇게 도는 동안 cards1에서 같은 단어를 만나면 집어 넣는다.
+            // 2. 현재 단어가 cards1에 있다면 해당 카드 뭉치를 사용한다. cards2에 있다면 cards 뭉치를 사용한다.
+            if (index < cards1.length && word.equals(cards1[index])) {
+                index++;
+            }
+            else if (index < cards2.length && word.equals(cards2[index])) {
+                index++;
+            }
+            else {
+                return "No";
+            }
 
-        // 3. cards1에 같은 단어가 없으면 cards2를 뒤져 같은 단어를 집어 넣는다.
-
-        // 4. 그렇게 해서 완성된 새로운 배열이 goal과 같으면 "Yes"를, 아니면 "No"를 반환한다.
-        return answer;
+            usedWords.add(word);
+        }
+        // 3. 모든 단어를 순서대로 사용했을 경우 "Yes" 반환한다.
+        return "Yes";
     }
 }

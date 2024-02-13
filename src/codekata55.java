6e3092ccec0270bf0d5c5c822ec8d25457ed3696
kuma0112 <lsj23871@gmail.com>
2024-02-13 14:22:24 +0900

    240213 코드카타 55번 카드 뭉치

--- src/codekata55.java
+++ src/codekata55.java
@@ -1,30 +1,59 @@
-import java.util.HashSet;
-
 public class codekata55 {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        HashSet<String> usedWords = new HashSet<>();
-        int index = 0;
-
+        // 각 카드 뭉치에서 현재 선택된 카드를 추적하는 인덱스
+        int index1 = 0, index2 = 0;
         for (String word : goal) {
-            // 1. 이미 사용한 단어인지 확인한다.
-            if (usedWords.contains(word)) {
-                return "No"; // 이미 사용한 단어일 경우 "No" 반환한다.
+            boolean found = false; // 단어가 카드 뭉치에서 찾아졌으면 true. 기본값은 false
+
+            // 1. 첫 번째 카드 뭉치에서 현재 단어를 찾음
+            if (index1 < cards1.length && word.equals(cards1[index1])) {
+                index1++;
+                found = true;
+            }
+            // 2. 두 번째 카드 뭉치에서 현재 단어를 찾음
+            else if (index2 < cards2.length && word.equals(cards2[index2])) {
+                index2++;
+                found = true;
             }
 
-            // 2. 현재 단어가 cards1에 있다면 해당 카드 뭉치를 사용한다. cards2에 있다면 cards 뭉치를 사용한다.
-            if (index < cards1.length && word.equals(cards1[index])) {
-                index++;
-            }
-            else if (index < cards2.length && word.equals(cards2[index])) {
-                index++;
-            }
-            else {
+            // 3. 현재 단어를 두 카드 뭉치에서 찾지 못했다면(한 개라도 false가 있다면), goal을 이룰 수 없으므로 No 반환
+            if (!found) {
                 return "No";
             }
-
-            usedWords.add(word);
         }
-        // 3. 모든 단어를 순서대로 사용했을 경우 "Yes" 반환한다.
+
+        // 4. 모든 단어를 찾았다면, goal 문장을 만들었으므로 Yes 반환
         return "Yes";
+
+        /*
+        Queue를 이용하여 찾는 방법
+        -> 각각의 queue를 만든 뒤, 서로 일치하면 하나씩 뽑음.
+        Queue<String> qGoal = new LinkedList<>();
+        Queue<String> qCard1 = new LinkedList<>();
+        Queue<String> qCard2 = new LinkedList<>();
+
+        for(String str : goal) {
+            qGoal.offer(str);
+        }
+
+        for(String card : cards1) {
+            qCard1.offer(card);
+        }
+
+        for(String card : cards2) {
+            qCard2.offer(card);
+        }
+
+        while(!qGoal.isEmpty()) {
+            String str = qGoal.poll();
+
+            if(str.equals(qCard1.peek())) qCard1.poll();
+            else if(str.equals(qCard2.peek())) qCard2.poll();
+            else return "No";
+        }
+
+        return "Yes";
+
+         */
     }
 }

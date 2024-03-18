e4b6c8f63ba3f739c2759414b3d4572272ff47e5
kuma0112 <lsj23871@gmail.com>
2024-03-18 20:59:12 +0900

    240318 코드카타 63번 숫자 짝꿍

--- src/codekata63.java
+++ src/codekata63.java
@@ -1,38 +1,34 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class codekata63 {
     public String solution(String X, String Y) {
-        String answer = "";
+        Map<Character, Integer> countX = new HashMap<>();
+        Map<Character, Integer> commonCounts = new HashMap<>();
 
-        ArrayList<Character> arrayX = new ArrayList<>();
-        ArrayList<Character> arrayY= new ArrayList<>();
-
-        for (int i = 0; i < X.length(); i++){
-            arrayX.add(X.charAt(i));
+        for (char ch : X.toCharArray()) {
+            countX.put(ch, countX.getOrDefault(ch, 0) + 1);
         }
 
-        for (int i = 0; i < Y.length(); i++){
-            arrayY.add(Y.charAt(i));
-        }
-
-        ArrayList<Character> commonChars = new ArrayList<>();
-        for (Character ch : arrayX) {
-            if (arrayY.contains(ch)) {
-                commonChars.add(ch);
-                arrayY.remove(ch);
+        for (char ch : Y.toCharArray()) {
+            if (countX.containsKey(ch) && countX.get(ch) > 0) {
+                commonCounts.put(ch, commonCounts.getOrDefault(ch, 0) + 1);
+                countX.put(ch, countX.get(ch) - 1); // 중복 방지
             }
         }
 
-        if (commonChars.isEmpty()) {
+        if (commonCounts.isEmpty()) {
             return "-1";
         }
 
-        Collections.sort(commonChars);
+        // 공통 문자들을 정렬하여 결과 문자열 생성
+        ArrayList<Character> commonChars = new ArrayList<>(commonCounts.keySet());
+        Collections.sort(commonChars, Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
-        for (Character ch : commonChars) {
-            sb.append(ch);
+        for (char ch : commonChars) {
+            int count = commonCounts.get(ch);
+            for (int i = 0; i < count; i++) {
+                sb.append(ch);
+            }
         }
 
         String result = sb.toString();

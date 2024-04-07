a238e05df7d46201959647b66ef6ac149823a393
kuma0112 <lsj23871@gmail.com>
2024-04-07 15:23:17 +0900

    240407 코드카타 68번 햄버거 만들기

--- src/codekata68.java
+++ src/codekata68.java
@@ -1,24 +1,28 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class codekata68 {
-    public int solution(int[] ingredient) {
-        int answer = 0;
-        // 1. int 배열을 arraylist에 넣기
-        ArrayList<Integer> array = new ArrayList<>();
-        for (int i = 0; i< ingredient.length; i ++) {
-            array.add(ingredient[i]);
-        }
+    public static void main(String[] args) {
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
 
-        for (int i = 0; i < array.size()-3; i++) {
-            if (array.get(i)== 1 && array.get(i+1) == 2 && array.get(i+2) == 3 && array.get(i+3) == 1) {
-                answer ++;
-                array.remove(i+3);
-                array.remove(i+2);
-                array.remove(i+1);
-                array.remove(i);
-                i = 0;
+        System.out.println(solution(ingredient));
+    }
+
+    public static int solution(int[] ingredient) {
+        int count = 0;
+        int[] array = new int[ingredient.length];
+        int index = 0;
+        for (int i : ingredient) {
+            array[index] = i;
+            if (index >= 3 && array[index-3] == 1
+                    && array[index-2] == 2
+                    && array[index-1] == 3
+                    && array[index] == 1) {
+                count ++;
+                index -= 4;
             }
+            index++;
         }
-        return answer;
+        return count;
     }
 }

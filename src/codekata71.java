e8303a09f9a2b3350b2a69865832db72c5056f76
kuma0112 <lsj23871@gmail.com>
2024-04-10 15:14:46 +0900

    240410 코드카타 71번 개인정보 수집 유효기간(푸는 중)

--- src/codekata71.java
+++ src/codekata71.java
@@ -1,47 +1,67 @@
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 public class codekata71 {
     public static void main(String[] args) throws ParseException {
         String today = "2022.05.19";
         String[] terms = {"A 6", "B 12", "C 3"};
         String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
-        System.out.println(Arrays.toString(solution(today, terms, privacies)));
+        System.out.println(solution(today, terms, privacies));
     }
-    public static int[] solution(String today, String[] terms, String[] privacies) throws ParseException {
-        int[] answer = {};
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd");
+    public static List<Integer> solution(String today, String[] terms, String[] privacies) throws ParseException {
+        List<Integer> answer = new ArrayList<>();
         // 1. Hashmap termsHashmap key는 terms의 앞 부분, value는 terms의 뒷 부분
         HashMap<String, Integer> termsHashmap = new HashMap<>();
-        for (int i = 0; i < terms.length; i++) {
-            String[] parts = terms[i].split(" ");
+        for (String term : terms) {
+            String[] parts = term.split(" ");
             termsHashmap.put(parts[0], Integer.parseInt(parts[1]));
         }
 
         // 2. privacies를 공백을 기준으로 앞 부분과 뒷 부분으로 나누어서 또다른 hashmap에 넣기
         // 이때, key가 뒷부분, value가 앞부분
-        HashMap<String, Date> privaciesHashmap = new HashMap<>();
-        for (int i = 0; i < privacies.length; i++) {
-            String[] parts = terms[i].split(" ");
-            String stringDate = parts[0];
-            Date date = formatter.parse(stringDate);
-            privaciesHashmap.put(parts[1], date);
+        HashMap<String, String> privaciesHashmap = new HashMap<>();
+        for (String privacy : privacies) {
+            String[] parts = privacy.split(" ", 2);
+            privaciesHashmap.put(parts[1], parts[0]);
         }
 
         // 3. today를 .을 기준으로 분리한다. year, month, day
-        Date dateToday =
+        String[] todayString = today.split("\\.");
+        int year = Integer.parseInt(todayString[0]);
+        int month = Integer.parseInt(todayString[1]);
+        int day = Integer.parseInt(todayString[2]);
 
         // 4. for문으로 privaciesHashmap을 돌면서 termsHashmap key와 privaciesHashmap의 key가 같으면
         // 해당 value인 개인정보 수집 일자를 year, month, day로 나눈 다음
         // 유효기간만큼 month에 더하고, day -1한다.
         // 그 결과, day가 0이면 day +28, month -1
         // month가 13 이상이면 month -12, year +1
-        // 그다음 today와 비교해서 수집 일자가 today를 넘기면 파기 대상이기 때문에, 해당 인덱스를 추적한다.
 
+        int check = 0;
+        for (String key : privaciesHashmap.keySet()){
+            if(termsHashmap.containsKey(key)) {
+                String[] privaciesString = privaciesHashmap.get(key).split("\\.");
+                int privaciesYear = Integer.parseInt(privaciesString[0]);
+                int privaciesMonth = Integer.parseInt(privaciesString[1]);
+                int privaciesDay = Integer.parseInt(privaciesString[2]);
+                privaciesMonth += termsHashmap.get(key);
 
+                if (privaciesMonth >= 13) {
+                    privaciesMonth -= 12;
+                    privaciesYear += 1;
+                }
+
+                if (privaciesYear < year || (privaciesYear == year && privaciesMonth < month)
+                        || (privaciesYear == year && privaciesMonth == month && privaciesDay < day)){
+                    for (int i = 0; i < privacies.length; i++) {
+                        if (privacies[i].endsWith(key)) {
+                            check = i;
+                        }
+                    }
+                }
+                answer.add(check);
+            }
+        }
         return answer;
     }
 }
